645768810ed49043991f61e733ef07848dfefacb
Rokhit Roy <rokhitroy2@gmail.com>
2026-08-19 11:25:40 +0530

    Time: 2059 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT product_id,year as first_year,quantity,price
+FROM Sales
+WHERE (year,product_id)=ANY (SELECT MIN(year) AS year,product_id
+FROM Sales 
+GROUP BY product_id);
